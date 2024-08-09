9f02ee90c3a83c9422e64a96ec5a3d4551c506fa
Batuhan Erdem <79407999+batuhanerdem@users.noreply.github.com>
2024-08-09 08:35:26 +0300

    Removed unnecessary annotations

--- app/src/main/java/com/example/weatherapp/di/RepositoryModule.kt
+++ app/src/main/java/com/example/weatherapp/di/RepositoryModule.kt
@@ -10,18 +10,15 @@ import dagger.Module
 import dagger.Provides
 import dagger.hilt.InstallIn
 import dagger.hilt.android.components.ViewModelComponent
-import javax.inject.Named
 
 @Module
 @InstallIn(ViewModelComponent::class)
 class RepositoryModule {
 
-    @CityNameApi
     @Provides
     fun provideCityNameRepository(service: CityNameService): CityNameRepository =
         CityNameRepositoryImpl(service)
 
-    @WeatherApi
     @Provides
     fun provideWeatherRepository(service: WeatherService): WeatherRepository =
         WeatherRepositoryImpl(service)
--- app/src/main/java/com/example/weatherapp/ui/MainActivityViewModel.kt
+++ app/src/main/java/com/example/weatherapp/ui/MainActivityViewModel.kt
@@ -3,8 +3,6 @@ package com.example.weatherapp.ui
 import android.util.Log
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
-import com.example.weatherapp.di.CityNameApi
-import com.example.weatherapp.di.WeatherApi
 import com.example.weatherapp.domain.repository.CityNameRepository
 import com.example.weatherapp.domain.repository.WeatherRepository
 import com.example.weatherapp.utils.SUCCESS
@@ -14,21 +12,21 @@ import javax.inject.Inject
 
 @HiltViewModel
 class MainActivityViewModel @Inject constructor(
-    @WeatherApi private val weatherRepository: WeatherRepository,
-    @CityNameApi private val cityNameRepository: CityNameRepository
+    private val weatherRepository: WeatherRepository,
+    private val cityNameRepository: CityNameRepository
 ) : ViewModel() {
     init {
         viewModelScope.launch {
             weatherRepository.getWeatherByName("sivas").collect {
-                it.data?.let {
-                    Log.d(SUCCESS, "weat: ${it}")
-                }
+//                it.data?.let {
+//                    Log.d(SUCCESS, "weat: ${it}")
+//                }
             }
             cityNameRepository.getCityByName("sivas").collect {
-//                Log.d(SUCCESS, "city: $it ")
-//                it.data?.let {
-//                    Log.d(SUCCESS, ": $it")
-//                }
+                Log.d(SUCCESS, "city: $it ")
+                it.data?.let {
+                    Log.d(SUCCESS, "city: $it")
+                }
             }
         }
     }
