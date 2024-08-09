ca8d17d93f1bc13895e4a65646f475b7705be8bd
Batuhan Erdem <79407999+batuhanerdem@users.noreply.github.com>
2024-08-09 11:37:58 +0300

    changed the SearchComponent ui structure

--- app/src/main/java/com/example/weatherapp/ui/search_component/SearchComponent.kt
+++ app/src/main/java/com/example/weatherapp/ui/search_component/SearchComponent.kt
@@ -27,6 +27,7 @@ import androidx.compose.ui.unit.dp
 import androidx.hilt.navigation.compose.hiltViewModel
 import androidx.lifecycle.compose.collectAsStateWithLifecycle
 import com.example.weatherapp.R
+import com.example.weatherapp.domain.model.City
 import com.example.weatherapp.ui.search_component.city_item.CityItem
 import com.example.weatherapp.ui.theme.Background
 import com.example.weatherapp.ui.theme.SearchBackground
@@ -38,31 +39,25 @@ fun SearchComponent(modifier: Modifier = Modifier) {
     val loadingState = viewModel.dataClass.loadingState.collectAsStateWithLifecycle()
     val errorState = viewModel.dataClass.errorState.collectAsStateWithLifecycle()
     val city = viewModel.dataClass.city.collectAsStateWithLifecycle()
-    val roundPair = remember { mutableStateOf(Pair(20, 20)) }
 
     var textState by remember { mutableStateOf(TextFieldValue("")) }
 
     LaunchedEffect(key1 = textState) {
         if (textState.text.isNotEmpty()) return@LaunchedEffect
         viewModel.setCityNull()
-        roundPair.value = Pair(20, 20)
     }
 
     Column(
         modifier = modifier
             .fillMaxWidth()
-            .padding(top = 10.dp)
+            .padding(horizontal = 25.dp, vertical = 10.dp)
+            .background(
+                Background, RoundedCornerShape(20.dp)
+            )
     ) {
 
         Row(
-            modifier = modifier
-                .fillMaxWidth()
-                .padding(horizontal = 25.dp)
-                .background(
-                    Background, RoundedCornerShape(
-                        20.dp, 20.dp, roundPair.value.first.dp, roundPair.value.second.dp
-                    )
-                ), verticalAlignment = Alignment.CenterVertically
+            modifier = modifier.fillMaxWidth(), verticalAlignment = Alignment.CenterVertically
         ) {
             Icon(painter = painterResource(id = R.drawable.ic_search),
                 contentDescription = "Search Icon",
@@ -70,8 +65,6 @@ fun SearchComponent(modifier: Modifier = Modifier) {
                 modifier = Modifier
                     .padding(horizontal = 15.dp)
                     .clickable { viewModel.getCity(textState.text) })
-
-//            Spacer(modifier = Modifier.width(8.dp))
 
             TextField(
                 value = textState,
@@ -94,28 +87,72 @@ fun SearchComponent(modifier: Modifier = Modifier) {
                     unfocusedContainerColor = Color.Transparent
                 ),
             )
+
         }
         city.value?.let {
-            roundPair.value = Pair(0, 0)
-            Column(
-                modifier = Modifier
-                    .fillMaxWidth()
-                    .padding(horizontal = 25.dp)
-//                    .fillMaxHeight(0.4f)
-                    .background(Background, shape = RoundedCornerShape(0.dp, 0.dp, 20.dp, 20.dp))
-            ) {
-                CityItem(city = it, modifier = Modifier.padding(top = 20.dp))
-            }
+            CityItem(city = it, modifier = Modifier.padding(top = 20.dp))
         }
-
-
     }
 
+
 }
+
 
 @Preview(showSystemUi = true, showBackground = true)
 @Composable
 private fun SearchComponentPreview() {
-    SearchComponent()
 
+    Column(
+        modifier = Modifier
+            .fillMaxWidth()
+            .padding(top = 10.dp)
+            .padding(horizontal = 25.dp)
+            .background(
+                Background, RoundedCornerShape(
+                    20.dp
+                )
+            )
+    ) {
+
+        Row(
+            modifier = Modifier.fillMaxWidth(), verticalAlignment = Alignment.CenterVertically
+
+        ) {
+            Icon(
+                painter = painterResource(id = R.drawable.ic_search),
+                contentDescription = "Search Icon",
+                tint = SearchBackground,
+                modifier = Modifier.padding(horizontal = 15.dp)
+            )
+
+//            Spacer(modifier = Modifier.width(8.dp))
+
+            TextField(
+                value = "",
+                onValueChange = {
+//                    textState = it
+                },
+                maxLines = 1,
+                placeholder = {
+                    Text(
+                        text = "Search for a location",
+                        color = SearchBackground,
+                    )
+                },
+                modifier = Modifier.fillMaxWidth(),
+                colors = TextFieldDefaults.colors(
+                    cursorColor = Color.Black,
+                    focusedIndicatorColor = Color.Transparent,
+                    unfocusedIndicatorColor = Color.Transparent,
+                    focusedContainerColor = Color.Transparent,
+                    unfocusedContainerColor = Color.Transparent
+                ),
+            )
+//            city.value?.let {
+//                roundPair.value = Pair(0, 0)
+//            }
+        }
+        CityItem(city = City("Sivas", "TR"))
+
+    }
 }
