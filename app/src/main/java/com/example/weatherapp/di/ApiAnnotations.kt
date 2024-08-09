3c5f95c3e53f36bcd888fe1e33b356c517f1013c
Batuhan Erdem <79407999+batuhanerdem@users.noreply.github.com>
2024-08-09 08:44:02 +0300

    @Named() wasn't needed at all.

--- app/src/main/java/com/example/weatherapp/di/ApiAnnotations.kt
+++ app/src/main/java/com/example/weatherapp/di/ApiAnnotations.kt
@@ -1,14 +1,11 @@
 package com.example.weatherapp.di
 
-import javax.inject.Named
 import javax.inject.Qualifier
 
-@Named("Weather")
 @Qualifier
 @Retention(AnnotationRetention.BINARY)
 annotation class WeatherApi
 
-@Named("CityName")
 @Qualifier
 @Retention(AnnotationRetention.BINARY)
 annotation class CityNameApi
