081afcdffba273155e11c7850509258abe9576ae
Batuhan <batuhanerdem173@gmail.com>
2024-08-22 01:15:40 +0300

    ApiKeys imported

--- app/src/main/java/com/example/weatherapp/data/service/CityNameService.kt
+++ app/src/main/java/com/example/weatherapp/data/service/CityNameService.kt
@@ -5,6 +5,7 @@ import retrofit2.Response
 import retrofit2.http.GET
 import retrofit2.http.Headers
 import retrofit2.http.Query
+import com.example.weatherapp.data.ApiKeys
 
 interface CityNameService {
     @Headers(Constants.X_API_KEY)
