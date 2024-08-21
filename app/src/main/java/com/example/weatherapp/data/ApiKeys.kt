1152496a98cdfddb867ad3b2803f351a897dcbcb
Batuhan <batuhanerdem173@gmail.com>
2024-08-22 01:18:06 +0300

    ApiKeys object added

---
+++ app/src/main/java/com/example/weatherapp/data/ApiKeys.kt
@@ -0,0 +1,6 @@
+package com.example.weatherapp.data
+
+object ApiKeys {
+//    const val CITY_NAME = "" your api from https://api-ninjas.com/api/city
+//    const val WEATHER = "" your api from https://www.weatherapi.com/
+}
