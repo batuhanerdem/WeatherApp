7216373c255a97ed27713a92df432ace84a88676
Batuhan <batuhanerdem173@gmail.com>
2024-08-22 01:08:52 +0300

    removed unnecessary comment lines

--- app/src/main/java/com/example/weatherapp/domain/model/weather/WeatherCurrent.kt
+++ app/src/main/java/com/example/weatherapp/domain/model/weather/WeatherCurrent.kt
@@ -2,11 +2,13 @@ package com.example.weatherapp.domain.model.weather
 
 data class WeatherCurrent(
     val condition: String,
-    val temperature: Double, // in °C
-    val feelsLike: Double, // in °C
-    val weatherDescription: String, val windSpeed: Double, // in km/h
-    val humidity: Int, // in percentage
-    val pressure: Double, // in mb
-    val visibility: Double, // in km
-    val uvIndex: Double, val gustSpeed: Double // in km/h
+    val temperature: Double,
+    val feelsLike: Double,
+    val weatherDescription: String,
+    val windSpeed: Double,
+    val humidity: Int,
+    val pressure: Double,
+    val visibility: Double,
+    val uvIndex: Double,
+    val gustSpeed: Double
 )
